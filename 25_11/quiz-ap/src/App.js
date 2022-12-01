575247063981a58e652144e57180ff2f5ec590b4
Abdulrahman Albarghouth <abdulrahmanalbarghouth@gmail.com>
2022-12-01 10:43:21 +0300

    25_11

---
+++ 25_11/quiz-ap/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Quiz from './components/Quiz';
+import Slider from './components/Slider/Slider';
+
+function App() {
+	return (
+		<div>
+			<Slider />
+			<Quiz />
+		</div>
+	);
+}
+
+export default App;
