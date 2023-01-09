6f6a24d3de0b0847f07895a13b945e149ea15017
Abdulrahman Albarghouth <58550474+Abdulrahman-Albarghouth@users.noreply.github.com>
2023-01-09 12:10:31 +0300

    06_01

---
+++ 06_01/companies-directory-api/transformers/admin.js
@@ -0,0 +1,15 @@
+const adminTransformer = (admin) => {
+    if (admin?.dataValues?.password) {
+        delete admin.dataValues.password
+    }
+    return admin
+}
+
+const adminsTransformer = (admins) => {
+    return admins.map((admin) => adminTransformer(admin))
+}
+
+module.exports = {
+    adminTransformer,
+    adminsTransformer
+}
